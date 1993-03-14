845f2813a08c1349312929aea3a473e4b7224565
Eugene Sandulenko <sev@scummvm.org>
1993-03-14 17:23:04 +0000

    Добавлено меню (не нагружено)

--- edmenu.c
+++ edmenu.c
@@ -1,10 +1,13 @@
 /*
- *  $Id: edmenu.c,v 1.1 1993/03/14 12:32:14 kas Exp $
+ *  $Id: edmenu.c,v 1.2 1993/03/14 17:23:04 sev Exp $
  *
  * ---------------------------------------------------------- 
  *
  * $Log: edmenu.c,v $
- * Revision 1.1  1993/03/14 12:32:14  kas
+ * Revision 1.2  1993/03/14 17:23:04  sev
+ * Добавлено меню (не нагружено)
+ *
+ * Revision 1.1  1993/03/14  12:32:14  kas
  * Initial revision
  *
  * Revision 1.1  1993/03/04  07:41:26  kas
@@ -13,17 +16,18 @@
  *
 */
 
-static char rcsid[]="$Id: edmenu.c,v 1.1 1993/03/14 12:32:14 kas Exp $";
+static char rcsid[]="$Id: edmenu.c,v 1.2 1993/03/14 17:23:04 sev Exp $";
 
-#define VCM_DEFS                /* Include Menu Defines and structures      */
-
+#define VCM_DEFS            /* Include Menu Defines and structures      */
+#define VCGET_DEFS
 #include <vcstdio.h>
+#include <vcdbms.h>
+#include <vced.h>
 
 /* ------------------------------------------------------------------------ */
 
-COUNT menu(vced,mode)
+COUNT edmenu(vced)
 VCED *vced;
-COUNT mode;
 {
 
 	COUNT vcmhf3();
@@ -37,29 +41,6 @@ COUNT mode;
 	VCMENU *textmenu;                       /*   "      "      "        */
 
 	MENUITEM *menuxitem();                  /* Function declaration     */
-	COUNT ednewf();
-	COUNT ednextf();
-	COUNT eddelf();
-	COUNT edsavef();
-	COUNT edir();
-	COUNT vcedabort();
-	COUNT edconf();
-	COUNT edhmark();
-	COUNT edvmark();
-	COUNT ed_del_conc();
-	COUNT edpaste();
-	COUNT edendmark();
-	COUNT edunmark();
-	COUNT edhelp();
-	COUNT edinf();
-        COUNT edseginf();
-	COUNT edfindu();
-       	COUNT edfindch();
-	COUNT edsys();
-        COUNT edgotom();
-        COUNT edcompil();
-        COUNT edshow();
-	COUNT eddrawch();
 	/***************/
 	COUNT cntrl_menu;
 	COUNT cntrl_parm;
@@ -97,42 +78,10 @@ COUNT mode;
 	cntrl_parm = STRPARM+SENDTXT+HIDE;
 	cntrl_escape = STRPARM+ESCAPE+SENDTXT+HIDE;
 
-	if(mode==POLE || mode==SEGM)
-	{
-	  ungetone(RET);
-	  cntrl_dir=cntrl_menu=UNAVAILABLE;
-	}
-	else
-	{                /*
-	    if(stat_hyp==PRIV_SEGWDO)
-	    {
-		ungetone(RET);
-		cntrl_pole=UNAVAILABLE;
-		cntrl_menu=UNAVAILABLE;
-		cntrl_dir=UNAVAILABLE;
-	     }
-
-	     else
-	     {
-			  */
-		 cntrl_pole=MENU;
-		 cntrl_dir=STRPARM+ESCAPE+SENDTXT+HIDE;
-		 cntrl_menu=MENU;
-
-		 cntrl_seg=MENU;
-	}
-	if(mode==POLE)
-	{
-	     cntrl_pole=MENU;
-	     cntrl_escape=UNAVAILABLE;
-	     cntrl_seg=UNAVAILABLE;
-	}
-	if(mode==SEGM)
-	{
-	     cntrl_seg=MENU;
-	     cntrl_escape=UNAVAILABLE;
-	     cntrl_pole=UNAVAILABLE;
-	}
+	cntrl_pole=MENU;
+	cntrl_dir=STRPARM+ESCAPE+SENDTXT+HIDE;
+	cntrl_menu=MENU;
+ 	cntrl_seg=MENU;
 
 	/********************************************************************/
 	/**************************  Add Menu Items *************************/
@@ -147,88 +96,79 @@ COUNT mode;
 	    NULLTEXT,"NO HELP",cntrl_seg);
 	menuxitem(mainmenu,"  Поле ",NULL,0,NULLFUNC,(TEXT *)polemenu,
 	    NULLTEXT,"NO HELP",cntrl_pole);
-	menuxitem(mainmenu," Cправочник ",NULL,0,edir,(TEXT *)vced,
+	menuxitem(mainmenu," Cправочник ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_dir);
-	menuxitem(mainmenu,"  Помощь  ",NULL,0,edhelp,(TEXT *)vced,
+	menuxitem(mainmenu,"  Помощь  ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
-	menuxitem(mainmenu,"  Выход  ",NULL,0,vcedabort,(TEXT *)vced,
+	menuxitem(mainmenu,"  Выход  ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
 
 	/************************** Files Menu Items ************************/
 
-	menuxitem(filesmenu,"Загрузить новый файл       F12 ",NULL,0,ednewf,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Загрузить новый файл       F12 ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		  "NO HELP",cntrl_escape);
-	menuxitem(filesmenu,"Список файлов              F11 ",NULL,0,edlistwdo,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Список файлов              F11 ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		  "NO HELP",cntrl_escape);
-	menuxitem(filesmenu,"Список файлов в каталоге   F13 ",NULL,0,edfiles,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Список файлов в каталоге   F13 ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		  "NO HELP",cntrl_escape);
-	menuxitem(filesmenu,"Сохранить текущий файл     F16 ",NULL,0,edsavef,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Сохранить текущий файл     F16 ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		 "NO HELP",cntrl_escape);
-	menuxitem(filesmenu,"Удалить файл из окна       F15 ",NULL,0,eddelf,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Удалить файл из окна       F15 ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		 "NO HELP",cntrl_escape);
-	menuxitem(filesmenu,"Переход к следующему файлу F14 ",NULL,0,ednextf,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Переход к следующему файлу F14 ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		 "NO HELP",cntrl_escape);
 	menuxitem(filesmenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
 		 "NO HELP",SEPARATOR);
-       	menuxitem(filesmenu,"Начальный  сегмент            ",NULL,0,edconf,(TEXT *)vced,NULL,
+       	menuxitem(filesmenu,"Начальный  сегмент            ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		 "NO HELP",cntrl_parm);
-	menuxitem(filesmenu,"Компиляция                    ",NULL,0,edcompil,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Компиляция                    ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		 "NO HELP",cntrl_parm);
-	menuxitem(filesmenu,"Просмотр                      ",NULL,0,edshow,(TEXT *)vced,NULL,
+	menuxitem(filesmenu,"Просмотр                      ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		 "NO HELP",cntrl_parm);
 
 	/*************************** SEG Menu Items ************************/
 
-	if(mode==SEGM)
-	{
-	    menuxitem(segmenu,"Oтметить сегмент ",NULL,0,edvmark,(TEXT *)vced,NULL,
+	    menuxitem(segmenu,"Oтметить сегмент ",NULL,0,NULLFUNC,(TEXT *)vced,NULL,
 		  "NO HELP",UNAVAILABLE);
 	    menuxitem(segmenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
 		 "NO HELP",SEPARATOR);
-	    menuxitem(segmenu,"Конец отметки сегмента ",NULL,0,edendmark,(TEXT *)vced,
+	    menuxitem(segmenu,"Конец отметки сегмента ",NULL,0,NULLFUNC,(TEXT *)vced,
 		      NULL,"NO HELP",STRPARM+SENDTXT+ESCAPE);
-	    menuxitem(segmenu,"Oтмена сегмента",NULL,0,edunmark,(TEXT *)vced,
+	    menuxitem(segmenu,"Oтмена сегмента",NULL,0,NULLFUNC,(TEXT *)vced,
 		      NULL,"NO HELP",STRPARM+SENDTXT+ESCAPE);
-	 }
-	else
-	    menuxitem(segmenu,"Oтметить сегмент       F9 ",NULL,0,edvmark,(TEXT *)vced,NULL,
-		  "NO HELP",cntrl_escape);
-	
-	    menuxitem(segmenu,"Информация о сегменте  УПР-w ",NULL,0,edseginf,(TEXT *)vced,NULL,
-		     "NO HELP",cntrl_escape);
+
 	/*************************** POLE Menu Items **********************/
 
-	menuxitem(polemenu,"Oтметить поле     F6 ",NULL,0,edhmark,(TEXT *)vced,NULLTEXT,
+	menuxitem(polemenu,"Oтметить поле     F6 ",NULL,0,NULLFUNC,(TEXT *)vced,NULLTEXT,
 	    "NO HELP",cntrl_escape);
-	menuxitem(polemenu,"Удалить поле      F7 ",NULL,0,ed_del_conc,(TEXT *)vced,
+	menuxitem(polemenu,"Удалить поле      F7 ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
-	menuxitem(polemenu,"Привязать поле    F8 ",NULL,0,edpaste,(TEXT *)vced,
+	menuxitem(polemenu,"Привязать поле    F8 ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
-	menuxitem(polemenu,"Информация о поле УПР-o ",NULL,0,edinf,(TEXT *)vced,
-	    NULLTEXT,"NO HELP",cntrl_escape);     	if(mode==POLE)
-	{
-	    menuxitem(polemenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
+	menuxitem(polemenu,"Информация о поле УПР-o ",NULL,0,NULLFUNC,(TEXT *)vced,
+	    NULLTEXT,"NO HELP",cntrl_escape);
+    menuxitem(polemenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
 		 "NO HELP",SEPARATOR);
-	    menuxitem(polemenu,"Конец отметки поля",NULL,0,edendmark,(TEXT *)vced,
+    menuxitem(polemenu,"Конец отметки поля",NULL,0,NULLFUNC,(TEXT *)vced,
 		      NULLTEXT,"NO HELP",STRPARM+SENDTXT+ESCAPE);
-	    menuxitem(polemenu,"Oтмена поля",NULL,0,edunmark,(TEXT *)vced,
+    menuxitem(polemenu,"Oтмена поля",NULL,0,NULLFUNC,(TEXT *)vced,
 		      NULLTEXT,"NO HELP",STRPARM+SENDTXT+ESCAPE);
-	 }
+
 	/*************************** TEXT Menu Items **********************/
 
-	menuxitem(textmenu,"Поиск строки      УПР-f ",NULL,0,edfindu,(TEXT *)vced,NULLTEXT,
+	menuxitem(textmenu,"Поиск строки      УПР-f ",NULL,0,NULLFUNC,(TEXT *)vced,NULLTEXT,
 	    "NO HELP",cntrl_escape);
-	menuxitem(textmenu,"Поиск с заменой   УПР-u ",NULL,0,edfindch,(TEXT *)vced,
+	menuxitem(textmenu,"Поиск с заменой   УПР-u ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
-	menuxitem(textmenu,"Перейти на строку УПР-g ",NULL,0,edgotom,(TEXT *)vced,
-	    NULLTEXT,"NO HELP",cntrl_escape);
-	menuxitem(textmenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
-		 "NO HELP",SEPARATOR);
-	menuxitem(textmenu,"Команда системе   УПР-k ",NULL,0,edsys,(TEXT *)vced,
+	menuxitem(textmenu,"Перейти на строку УПР-g ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
 	menuxitem(textmenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
 		 "NO HELP",SEPARATOR);
-	menuxitem(textmenu,"Рисование символов  F20 ",NULL,0,eddrawch,(TEXT *)vced,
+	menuxitem(textmenu,"Команда системе   УПР-k ",NULL,0,NULLFUNC,(TEXT *)vced,
+	    NULLTEXT,"NO HELP",cntrl_escape);
+	menuxitem(textmenu,NULLTEXT,NULL,0,NULLFUNC,NULL,NULL,
+		 "NO HELP",SEPARATOR);
+	menuxitem(textmenu,"Рисование символов  F20 ",NULL,0,NULLFUNC,(TEXT *)vced,
 	    NULLTEXT,"NO HELP",cntrl_escape);
 
 
--- medtsub.c
+++ medtsub.c
@@ -1,11 +1,14 @@
 /*
- *  $Id: medtsub.c,v 1.1 1993/03/14 12:32:14 kas Exp $
+ *  $Id: medtsub.c,v 1.2 1993/03/14 17:23:04 sev Exp $
  *
  *  --------------------------------------------------------------------------
  *
  *  $Log: medtsub.c,v $
- *  Revision 1.1  1993/03/14 12:32:14  kas
- *  Initial revision
+ *  Revision 1.2  1993/03/14 17:23:04  sev
+ *  Добавлено меню (не нагружено)
+ *
+ * Revision 1.1  1993/03/14  12:32:14  kas
+ * Initial revision
  *
  *
  *
@@ -15,6 +18,7 @@
 #include <vcstdio.h>
 #include <vcdbms.h>
 #include <vced.h>
+#include "mtdefs.h"
 
 /**
 *
@@ -119,81 +123,3 @@ VCED *vced;                 /* Pointer to edit structure            */
         }                               /*                          */
     return(retval);                     /* Return function address  */
     }                                   /* ------------------------ */
-
-/* ---------------------------------------------------------------- */
-/**
-*
-*   Name            vcedcmds.c - vced command structure
-*                                   
-*   Description     Tced command structure
-*
-**/
-
-#define VCEDVARS
-
-extern COUNT vcedmvleft();
-extern COUNT vcedmvright();
-extern COUNT vcedmvup();
-extern COUNT vcedmvdown();
-extern COUNT vcedexit();
-extern COUNT vcedprtbuf();
-extern COUNT vcedinstog();
-extern COUNT vcedbksp();
-extern COUNT vceddelete();
-extern COUNT vcedtol();
-extern COUNT vcedeol();
-extern COUNT vcedtogstat();
-extern COUNT vcedreturn();
-extern COUNT vcedabort();
-extern COUNT vcedhelp();
-extern COUNT vcedpgup();
-extern COUNT vcedpgdn();
-extern COUNT vcedtab();
-extern COUNT vcedbktab();
-extern COUNT vcedsbuf();
-extern COUNT vcedtof();
-extern COUNT vcedeof();
-extern COUNT vcedhome();
-extern COUNT vcedend();
-/* ------------------------------------------------------------------------ */
-
-extern COUNT vceddelln();
-extern COUNT vcedinsln();
-extern COUNT vceddeol();
-extern COUNT vcedmkpg();
-
-/* ------------------------------------------------------------------------ */
-
-VCEDCMD vcedcmds[] =
-    {
-    (TEXT *)"abort",        4000,vcedabort,     /* АР2    - Abort           */
-    (TEXT *)"backspace",    4001,vcedbksp,      /* ВШ     - BackSpace       */
-    (TEXT *)"backtab",      4005,vcedbktab,     /* ПС     - BackTab         */
-    (TEXT *)"begin_of_line",5010,vcedtol,       /* Ф10    - Begin of line   */
-    (TEXT *)"begin_of_file",4010,vcedtof,       /* ПТ     - Begin of file   */
-    (TEXT *)"cursor_down",  4009,vcedmvdown,    /*        ^                 */
-    (TEXT *)"cursor_left",  4006,vcedmvleft,    /*     <--|                 */
-    (TEXT *)"cursor_right", 4007,vcedmvright,   /*        |-->              */
-    (TEXT *)"cursor_up",    4008,vcedmvup,      /*        V                 */
-    (TEXT *)"delete_char",  4015,vceddelete,    /* УТ     - Delete char     */
-    (TEXT *)"delete_char",   127,vceddelete,    /*                          */
-    (TEXT *)"end_of_line",  5011,vcedeol,       /* ПФ14   - End of line     */
-    (TEXT *)"end_of_file",  4011,vcedeof,       /* ВНТ    - End of file     */
-    (TEXT *)"exit",         5003,vcedexit,      /* PF4    - Exit            */
-    (TEXT *)"menu",         5000,vcedhelp,      /* ПФ1    - Help            */
-    (TEXT *)"page_down",    4013,vcedpgdn,      /* ВСС    - Page down       */
-    (TEXT *)"page_up",      4012,vcedpgup,      /* ВПС    - Page up         */
-    (TEXT *)"return",       4002,vcedreturn,    /* ВК     - Return          */
-    (TEXT *)"save_buffer",  5002,vcedsbuf,      /* ПФ3    - Save file       */
-    (TEXT *)"tab",          4004,vcedtab,       /* ГТ     - Tab             */
-    (TEXT *)"toggle_insert",4014,vcedinstog,    /* РТ     - Insert/Over     */
-    (TEXT *)"toggle_status",5013,vcedtogstat,   /* Ф18    - Off/On status   */
-/* ------------------------------------------------------------------------ */
-    (TEXT *)"delete line",    12,vceddelln,     /* УПР l  - Delete line     */
-    (TEXT *)"insert line",    22,vcedinsln,     /* УПР v  - Insert line     */
-    (TEXT *)"delete eol",      5,vceddeol,      /* УПР e  - Delete end line */
-    (TEXT *)"mark page",       3,vcedmkpg,      /* УПР c  - Mark page       */
-/* ------------------------------------------------------------------------ */
-    NULLTEXT,                  0,(PFI)0,
-    };
-
