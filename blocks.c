f377d573ab8711aafa472ac4cf737fa9746d0560
Eugene Sandulenko <sev@scummvm.org>
1993-04-02 16:17:11 +0000

    Теперь ввод-вывод блоков осуществляется через временный файл. Еще есть баги

--- blocks.c
+++ blocks.c
@@ -1,10 +1,14 @@
 /*
- *  $Id: blocks.c,v 1.3 1993/03/29 12:06:38 sev Exp $
+ *  $Id: blocks.c,v 1.4 1993/04/02 16:17:11 sev Exp $
  *
  * ---------------------------------------------------------- 
  *
  * $Log: blocks.c,v $
- * Revision 1.3  1993/03/29 12:06:38  sev
+ * Revision 1.4  1993/04/02 16:17:11  sev
+ * Теперь ввод-вывод блоков осуществляется через временный файл. Еще
+ * есть баги
+ *
+ * Revision 1.3  1993/03/29  12:06:38  sev
  * Эта глупая штука не хочет работать из-за двух закоментаренных
  * строк ф функции инициализации
  *
@@ -22,27 +26,20 @@
 
 #include "mted.h"
 
+char cutbuf[80];
+
 COUNT edstartcut()
 {
-  static TEXT cutname[] = "cutbuf";
+  pid_t mted_pid;
   
-  cutbuf = (VCEDBUF *)vcalloc(1, sizeof(VCEDBUF *), vcdebug);
+  mted_pid = getpid();
 
-  if((cutbuf->bfdnum = dbmsxopn(-1,sizeof(VCEDLINE) + cutbuf->bflinelen,
-	0,1,0,vcedval.pages,vcedval.slots)) == (DBP)0)
-  {
-     askmsg("Editor Error","Can't alloc cut buffer",0);
-     return 0;
-  }
-  cutbuf->bfname = cutname;
-/*  cutbuf->bfbuffer = NULLTEXT;
-  cutbuf->bfchange = 0;
-*/  cutbuf->bfperm = 0;
-  cutbuf->bflinelen = vcedval.linlen;
-  cutbuf->bffline = (DBDP)0;
-  cutbuf->bfbline = (DBDP)0;
-  cutbuf->bfnlines = 0l;
-  cutbuf->bfctrl = VCEDFFIL;
+  sprintf(cutbuf, "/tmp/mted%d", mted_pid);
+}
+
+COUNT edendcut()
+{
+  remove(cutbuf);
 }
 
 COUNT begin_mark(vced)
@@ -58,13 +55,18 @@ VCED *vced;
 {
   DBDP i, lptr;
   VCEDLINE *line;
+  FILE *cut;
 
-/*  for(i = vced->edaline; i <= vced->edcline; i++)
+  cut = fopen(cutbuf, "w");
+  line = vcedgline(vced->edbuffer, vced->edaline);
+
+  for(i = vced->edaline; i <= vced->edcline; i++)
   {
-    line = vcedgline(vced->edbuffer, vced->edaline);
-    vcedaline(cutbuf, line->ltext, line->lused);
-*/
-  vcedaline(cutbuf, "string", 7);
+    fputs(line->ltext, cut);
+    fputc('\n', cut);
+    line = vcednline(vced->edbuffer, line);
+  }
+  fclose(cut);
 
   vced->edaline = (DBDP)0;
   vced->edupval = VCEDUPDALL;
@@ -73,26 +75,57 @@ VCED *vced;
 COUNT cut_block(vced)
 VCED *vced;
 {
+  DBDP i, lptr, lptr1;
+  VCEDLINE *line, *linetokill;
+  FILE *cut;
+
+  cut = fopen(cutbuf, "w");
+  line = vcedgline(vced->edbuffer, vced->edaline);
+  lptr = vced->edaline;
+
+  for(i = vced->edaline; i <= vced->edcline; i++)
+  {
+    fputs(line->ltext, cut);
+    fputc('\n', cut);
+    linetokill = line;
+    lptr1 = line->lnext;
+    line = vcednline(vced->edbuffer, line);
+    vceddline(vced->edbuffer, linetokill->lprev, lptr, linetokill->lnext);
+    lptr = lptr1;
+  }
+  fclose(cut);
+
+  vced->edcline = lptr;
+
+  vced->edaline = (DBDP)0;
+  vced->edupval = VCEDUPDALL;
 }
 
 COUNT paste_block(vced)
 VCED *vced;
 {
   VCEDLINE *line;
-  DBDP prev, next;
-  int i;
+  DBDP prev, next; 
+  FILE *cut;
+  char buf[1024];
+  DBDP old;
 
-/*
+  cut = fopen(cutbuf, "r");
   line = vcedgline(vced->edbuffer, vced->edcline);
-  prev = line->lprev;
-  next = line->lnext;
-*/
-  line = vcedgline(cutbuf, 1);
+  prev = old = line->lprev;
+  next = vced->edcline;
 
-  for(i = 0; i < cutbuf->bfnlines; i++, prev ++, next ++)
+  while(fgets(buf, 1024, cut))
   {
-    vcedaline(vced->edbuffer, line->ltext, line->lused);
-    line = vcednline(cutbuf, line);
-  }    
+    vcediline(vced->edbuffer, prev, next, buf, strlen(buf));
+    line = vcedgline(vced->edbuffer, vced->edcline);
+    prev = line->lprev;
+  }
+
+  fclose(cut);
+
+  line = vcedgline(vced->edbuffer, old);
+  vced->edcline = line->lnext;
+
   vced->edupval = VCEDUPDALL;
 }
