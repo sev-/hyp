01c7f8aadc067f5b284d35f02e9692709dda74fe
Eugene Sandulenko <sev@scummvm.org>
1993-06-08 12:09:42 +0000

    Полностью измененная программа

--- compiler.c
+++ compiler.c
@@ -1,321 +1,290 @@
 /*
- *  $Id: compiler.c,v 1.4 1993/05/25 17:22:43 sev Exp $
+ *  $Id: compiler.c,v 1.5 1993/06/08 12:09:42 sev Exp $
  *
  * ---------------------------------------------------------- 
  *
  * $Log: compiler.c,v $
- * Revision 1.4  1993/05/25 17:22:43  sev
- * Кой-чего тут подправил несущественное
- *
- * Revision 1.3  1993/05/24  13:47:39  sev
- * Убрал обидную ошибку в отчетк
- * 	ю
- *
- * Revision 1.2  1993/05/24  13:40:10  sev
- * Добавлен новый ключ -f - компиляция только одного файла
- *
- * Revision 1.1  1993/03/01  16:00:44  sev
- * Initial revision
+ * Revision 1.5  1993/06/08 12:09:42  sev
+ * Полностью измененная программа
  *
  *
  */
 
-static char rcsid[]="$Id: compiler.c,v 1.4 1993/05/25 17:22:43 sev Exp $";
-
-/*
-	Файл compiler.c
-	Запорожье 1993.
-*/
-
-#define BUF 1024
-#define PATH 80
-
-
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
 
-/*============================================================*/
-/*================ Начало главной программы ==================*/
-/*============================================================*/
-main(argc,argv)       /*         begin of main-programm       */
-int  argc;
-char **argv;
+#define PATHLEN 256
+#define BUF	1024
+
+typedef struct FILESTACK	/* стэк файлов */
 {
+  char			 path[32];
+  long			 position;	/* текущий offset в файле */
+  int			 pos_in_string;	/* последняя обработанная колонка */
+  struct FILESTACK	*previous;
+} FILESTACK;
 
-  struct stat STATBUF[1];
+FILESTACK *current;
+int fail = 0;
 
-  FILE *fopen();
-  FILE *in_file_config;
-  FILE *in_file_table;
-  FILE *current_file;
+void	 go_conf();
+void	 refer();
+void	 free_stack();
+char	*find();
+int	 newfile();
+int	 oldfile();
+char	*parser();
 
-  char string[BUF];   /*           Строка из файла            */
-  char path_file[PATH];
-  char name_file[PATH];
-  char work[PATH];
-  char path[PATH];
-  char *pointer;
-  char *strstr();
-  char *strchr();
-  char *strcat();
-  char *strncpy();
-  char *strcpy();
-  char *strrchr();
+main(argc, argv)
+int	argc;
+char  **argv;
+{
+  int carg;
 
-  time_t last_time_modify_config;
-  time_t last_time_modify;
-
-  long main_pointer_in_file;
-  long current_pointer_in_file;
-  int flag=0;
-  int  counter;
-  int  more;
-  int  yes_no_compile;
-  int  set;
-  int  not_all_compile;
-
-  void compile();
-
-
-  if(argc<2)         /*      Если нет входных параметров     */
+  if(argc < 2)
   {
-    puts("Компилятор гипертекста. Версия 1.1\n\n\
+    puts("Компилятор гипертекста. Версия 2.0\n\n\
  Использование:\n\t\tcompiler [-f] file\n");
-    exit(0);          /*             Выход в DOS              */
+    return 1;
   }
 
-  if(!strcmp(argv[1], "-f"))
+  carg = 1;
+  while(carg < argc)
   {
-    if( (current_file = fopen(argv[2],"r+")) == (FILE *)NULL )
+    go_conf(argv[carg]);
+    ++carg;
+    if(fail)
     {
-      printf(" Не могу открыть файл %s\n",argv[2]);
-      exit(1);
+      printf("Компиляция прошла не полностью!\n");
+      fail = 0;
     }
-
-    printf("Компилируется файл : %s",argv[2]);
-    fflush(stdout);
-    fseek(current_file,0l,SEEK_SET);
-    compile(&current_file);
-    printf("\rОткомпилирован файл : %s\n",argv[2]);
-    fclose(current_file);
-
-    exit(0);
+    else
+      printf("Компиляция прошла успешно!\n");
   }
 
-  if((in_file_config=fopen(argv[1],"r+"))==(FILE *)NULL)
-  {
-    printf(" Ошибка :не могу открыть файл %s\n",argv[1]);
-    exit(0);          /*             Выход в DOS              */
-  }
-
-  main_pointer_in_file    = 0l;/*                             */
-  last_time_modify_config = 0; /*      Инициализация          */
-  yes_no_compile          = 1; /*        переменных           */
-  counter                 = 0; /*                             */
-
-  while( fgets(string,BUF,in_file_config) != (char *)NULL )
-  {
-    if( counter == 0 )
-    {
-      strcpy(path_file,string);
-
-      while( strchr(path_file,'\\') )
-        *(strchr(path_file,'\\'))='/';
-
-      memset(path,'\0',PATH);
-      strncpy(path,path_file,
-              strlen(path_file)-strlen(strrchr(path_file,'/'))+1);
-      counter ++;
-    }
-    if( strstr(string,"for compiler : ")!=(char *)NULL)
-    {
-      sscanf(string,"for compiler : %ld",&last_time_modify_config);
-      flag=1;
-      break;
-    }
-    main_pointer_in_file = ftell(in_file_config);
-  }
-  flag=(flag==1)?0:1;
-  if( last_time_modify_config == 0 )
-  {
-    stat(argv[1],STATBUF);
-    last_time_modify_config = STATBUF->st_mtime;
-    fseek(in_file_config,main_pointer_in_file,SEEK_SET);
-    fprintf(in_file_config,"for compiler : %ld\n",last_time_modify_config);
-    if( ( in_file_table=fopen("ft_hyp","a") ) == (FILE *)NULL )
-    {
-      printf(" Ошибка :нет места на диске\n");
-      exit(0);        /*             Выход в DOS              */
-    }
-
-    fclose(in_file_table);
-    in_file_table=fopen("ft_hyp","r+");
-    fprintf(in_file_table,"%d%s",yes_no_compile,path_file);
-  }
-  else
-  {
-    if( ( in_file_table=fopen("ft_hyp","r+") ) == (FILE *)NULL )
-    {
-      if( ( in_file_table=fopen("ft_hyp","a+") ) == (FILE *)NULL )
-      {
-        printf(" Ошибка :нет места на диске\n");
-        exit(0);        /*             Выход в DOS              */
-      }
-      else
-      {
-        fclose(in_file_table);
-        in_file_table=fopen("ft_hyp","r+");
-        fprintf(in_file_table,"%d%s",yes_no_compile,path_file);
-      }
-    }
-  }
-
-  main_pointer_in_file = 0l;
-  counter               = 0;
-  fseek(in_file_table,0l,SEEK_SET);
-
-  while( fgets(string,BUF,in_file_table) != (char *)NULL )
-  {
-    if( string[0] == '1' )
-    {
-      counter ++;
-      break;
-    }
-  }
-
-  if( counter == 0 )
-    more = 0;
-  else
-    more = 1;
-
-  not_all_compile = 0;
-
-  while( more )
-  {
-    fseek(in_file_table,main_pointer_in_file,SEEK_SET);
-
-    if( fgets(string,BUF,in_file_table) == (char *)NULL )
-    {
-      more = 0;
-      continue;
-    }
-
-    current_pointer_in_file = ftell(in_file_table);
-    strcpy(path_file,&string[1]);
-    *(strchr(path_file,'\n')) = '\0';
-
-    if( (current_file = fopen(path_file,"r+")) == (FILE *)NULL )
-    {
-      printf(" Не могу открыть файл %s\n",path_file);
-      main_pointer_in_file = current_pointer_in_file;
-      not_all_compile = 1;
-      remove("ft_hyp");
-      continue;
-    }
-
-    while( ! feof( current_file ))
-    {
-      fgets(string,BUF,current_file);
-
-      if( pointer = strstr(string,"\033<") )
-      {
-        pointer = strrchr(pointer,';');
-        sscanf(pointer,";%s>",work);
-        *(strchr(work,'>')) = '\0';
-        strcpy(name_file,path);
-        strcat(name_file,work);
-
-        if( strchr(name_file,'*') )
-        {
-          counter = 1;
-          set = SEEK_END;
-        }
-        else
-        {
-          set = SEEK_SET;
-          counter = 0;
-        }
-
-        fseek(in_file_table,0l,set);
-
-        while( fgets(string,BUF,in_file_table) != (char *)NULL )
-          if( strstr(string,name_file) )
-          {
-            counter ++;
-            break;
-          }
-
-        if( counter == 0 )
-        {
-          fseek(in_file_table,0l,SEEK_END);
-          fprintf(in_file_table,"%d%s\n",yes_no_compile,name_file);
-        }
-      }
-    }
-
-    stat(path_file,STATBUF);
-    last_time_modify = STATBUF->st_mtime;
-
-    if(( (last_time_modify > last_time_modify_config)) ||( flag==1))
-    {
-      printf("Компилируется файл : %s",path_file);
-      fflush(stdout);
-      fseek(current_file,0l,SEEK_SET);
-      compile(&current_file);
-      printf("\rОткомпилирован файл : %s\n",path_file);
-    }
-/*    else
-      printf("Файл %s был уже откомпилирован \n",path_file);
-*/
-    fseek(in_file_table,main_pointer_in_file,SEEK_SET);
-    yes_no_compile = 0;
-    fprintf(in_file_table,"%d",yes_no_compile);
-    yes_no_compile = 1;
-    main_pointer_in_file = current_pointer_in_file;
-    fclose(current_file);
-  }
-
-  last_time_modify_config =  0;
-  main_pointer_in_file    = 0l;
-  fseek( in_file_config,0l,SEEK_SET );
-
-  if ( !not_all_compile )
-    while( fgets(string,BUF,in_file_config) != (char *)NULL )
-    {
-      if( strstr(string,"for compiler : ") )
-      {
-        fseek( in_file_config,main_pointer_in_file,SEEK_SET );
-        fprintf(in_file_config,"for compiler : %0.9ld",
-                last_time_modify_config);
-        printf(" Компиляция прошла успешно.\n ");
-        fclose(in_file_table);
-        fclose(in_file_config);
-        remove("ft_hyp");
-        exit(0);
-      }
-      main_pointer_in_file = ftell(in_file_config);
-    }
-  else
-  {
-    fclose(in_file_table);
-    fclose(in_file_config);
-    printf(" Компиляция произведена не полностью.\n ");
-  }
   return 0;
-}                     /*          end of main-programm        */
-/*============================================================*/
-/*================ Конец главной программы ===================*/
-/*============================================================*/
+}
 
+void go_conf(filename)	/* первая строка в файле - имя для обработки */
+char *filename;
+{
+  FILE *conf_file;
+  FILE *curr_file;
+  char *begin_file;
+  char  path[PATHLEN];
 
+  if((conf_file = fopen(filename, "r")) == (FILE *)NULL)
+  {
+    printf("Не могу открыть файл %s для чтения.\n", filename);
+    fail = 1;
+    return;
+  }
 
-/*------------------------------------------------------------*/
-/*-------------- Начало подпрограммы компиляции --------------*/
-/*------------------------------------------------------------*/
-void compile(in_file) /*          begin of subroutine         */
-FILE *(*in_file);        /*              Входной файл            */
+  fgets(path, PATHLEN, conf_file);	/* берем имя файла */
+
+  path[strlen(path)-1] = '\0';	/* убираем \n */
+  if((curr_file = fopen(path, "r")) == (FILE *)NULL)
+  {
+    printf("Неправильный файл %s.\n", filename);
+    fclose(conf_file);
+    return;
+  }
+
+  fclose(conf_file);
+  fclose(curr_file);
+
+  begin_file = (char *)strrchr(path, '/');	/* выделние пути и имени */
+  *(begin_file ++) = '\0';
+
+  refer(begin_file, path);
+
+  free_stack();
+}
+
+void refer(filein, path)	/* функция обработки файла filein */
+char *filein;				/* имя входного файла _БЕЗ ПУТИ_  */
+char *path;				/* путь к входному файлу	  */
+{
+  FILE *in;
+  char	buf[1024];
+  long	position;
+  char *where;
+  char	curr_file[256];	/* Имя текущего файла	*/
+
+  current = (FILESTACK *)malloc(sizeof(FILESTACK));
+  current->previous = (FILESTACK *)NULL;
+
+  strcpy(current->path, filein);
+  current->position = 0;
+  current->pos_in_string = 0;
+
+loop:
+
+  if(path != (char *)NULL)	/* сделаем имя файла */
+    sprintf(curr_file, "%s/%s", path, current->path);
+  else
+    strcpy(curr_file, current->path);
+
+  compile(curr_file);
+  in = fopen(curr_file, "r");
+
+loop1:
+
+  fseek(in, current->position, SEEK_SET);
+		/* второй проход. Теперь ищутся ссылки на файлы */
+  fgets(buf, 1024, in);
+
+  if((where = find(buf + current->pos_in_string, "\033<")) && !oldfile(where))
+  {
+    current->pos_in_string = where - buf;	/* сначала в строке, на */
+    if(newfile(current, where, path))		/* которой закончили */
+    {
+      fclose(in);
+      goto loop;	/* откроем новый файл */
+    }
+    goto loop1;
+  }
+
+  position = ftell(in);
+				/* теперь в остатке файла */
+  while(!(where = find(buf, "\033<")) && fgets(buf, 1024, in))
+    position = ftell(in);
+
+  if(where && !oldfile(where))	/* если файл не встречался раньше */
+  {
+    current->position = position;
+    current->pos_in_string = where - buf;
+    if(newfile(current, where, path))		/* которой закончили */
+    {
+      fclose(in);
+      goto loop;	/* откроем новый файл */
+    }
+    goto loop1;
+  }
+
+  if(current->previous != (FILESTACK *)NULL)	/* перейдем к предыдущему, */
+  {						/* если есть 		   */
+    current = current->previous;
+    fclose(in);
+
+    if(path != (char *)NULL)
+      sprintf(curr_file, "%s/%s", path, current->path);
+    else
+      strcpy(curr_file, current->path);
+
+    if((in = fopen(curr_file, "r")) != (FILE *)NULL)
+    {
+      fseek(in, current->position, SEEK_SET);
+      goto loop1;
+    }
+  }
+
+  fclose(in);
+}
+
+int oldfile(string)	/* пробегает по списку файлов */
+char *string;		/* в поисках такого же имени  */
+{
+  FILESTACK *tmpf;
+  char	    *name;
+
+  name = parser(string);
+  tmpf = current;
+  while(strcmp(tmpf->path, name) && tmpf->previous != (FILESTACK *)NULL)
+    tmpf = tmpf->previous;
+
+  if(!strcmp(tmpf->path, name))	/* этот файл уже обрабатывали */
+    return 1;
+
+  return 0;
+}  
+
+int newfile(old, string, path)	/* выделяет память для структуры и связывает */
+FILESTACK *old;			/* ее со списком */
+char	  *string, *path;
+{
+  FILESTACK *tmph;
+  char	    *name;
+  char	curr_file[256];	/* Имя текущего файла	*/
+  FILE *tmpfil;
+
+  name = parser(string);	/* выделим имя файла */
+
+  if(path != (char *)NULL)	/* сделаем имя файла */
+    sprintf(curr_file, "%s/%s", path, name);
+  else
+    strcpy(curr_file, name);
+
+  if((tmpfil = fopen(curr_file, "r+")) == (FILE *)NULL)
+  {
+    printf("Не могу открыть фалй %s.\n", curr_file);
+    fail = 1;
+    return 0;
+  }
+  fclose(tmpfil);
+  tmph = (FILESTACK *)malloc(sizeof(FILESTACK));	/* теперь память */
+
+  tmph->previous = old;		/* свяжем его */
+
+  strcpy(tmph->path, name);
+
+  tmph->position = 0;
+  tmph->pos_in_string = 0;
+
+  current = tmph;
+
+  return 1;
+}
+
+char *parser(string)	/* выделяет из строки вида dd;dd;string;string> */
+char *string;		/* последнюю строку */
+{
+  int	     x;
+  char	     b[PATHLEN];
+  char	     name[PATHLEN];
+  char	    *p;
+  
+  sscanf(string, "%d-%d;%s", &x, &x, b);
+  sscanf((char *)strchr(b, ';'), ";%s", name);
+  p = (char *)strchr(name, '>');
+  *p = '\0';
+
+  return name;
+}
+
+void free_stack()	/* освобождает память, выделенную под структуры */
+{
+  FILESTACK *tmpf;
+
+  while(current->previous != (FILESTACK *)NULL)
+  {
+    tmpf = current->previous;
+    free(current);
+    current = tmpf;
+  }
+  free(current);
+}
+
+char *find(string, pattern)	/* поиск подстроки. Возвращает указатель на */
+char *string;			/* конец найденной подстроки или 0 */
+char *pattern;
+{
+  char *p;
+
+  p = pattern;
+
+  while(*p && *string)
+  {
+    if(*string++ != *p++)
+      p = pattern;
+  }
+  if(!*p)
+    return string;
+  else
+    return 0;
+}
+
+compile(name)
+char *name;
 {
   char string[BUF];   /*           Строка из файла            */
 
@@ -323,32 +292,35 @@ FILE *(*in_file);        /*              Входной файл            */
   long current_work;  /*          в текущей странице          */
   long previous_pg;   /*     Указатель на начальную строку    */
   long previous_work; /*         в предыдущей странице        */
-  long ftell();
 
+  FILE *in_file;
   current_pg = 0;     /*                                      */
   previous_pg = 0;    /*      Инициализация переменных        */
   previous_work = 0;  /*                                      */
   current_work = 0;   /*                                      */
 
-  while( fgets(string,BUF,*in_file) != (char *)NULL )
-	{
+  if((in_file = fopen(name, "r+")) == (FILE *)NULL)
+    return 0;
 
-    if( strchr(string,'\014') == (char *)NULL ) /*   Поиск символа    */
+  printf("Компилируется файл %s", name);
+
+  while( fgets(string,BUF,in_file) != (char *)NULL )
+  {
+
+    if((char *)strchr(string,'\014') == (char *)NULL) /*   Поиск символа    */
       continue;                         	/*   новой сраницы    */
 
-      previous_work=current_pg;         	/*     Запомнить      */
-      current_pg=previous_pg;           	/*     указатель      */
-      previous_pg=ftell(*in_file);      	/*    на страницу     */
+    previous_work=current_pg;         	/*     Запомнить      */
+    current_pg=previous_pg;           	/*     указатель      */
+    previous_pg=ftell(in_file);      	/*    на страницу     */
 
-    current_work=ftell(*in_file);
-    fseek(*in_file,current_work-strlen(string),SEEK_SET);
-    fprintf(*in_file,"\014%8ld\n",previous_work);/*  Запись  */
+    current_work=ftell(in_file);
+    fseek(in_file,current_work-strlen(string),SEEK_SET);
+    fprintf(in_file,"\014%8ld\n",previous_work);/*  Запись  */
                                                  /*  в  файл */
-    fseek(*in_file,current_work,SEEK_SET);
-
-  }                   /*                while                 */
-  return;
-}                     /*            end of subroutine         */
-/*------------------------------------------------------------*/
-/*-------------- Конец подпрограммы компиляции ---------------*/
-/*------------------------------------------------------------*/
+    fseek(in_file,current_work,SEEK_SET);
+  }
+  fclose(in_file);
+  printf("\rОткомпилирован файл %s\n", name);
+  return 1;
+}
